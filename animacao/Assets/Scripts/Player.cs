102cc61cc43874a298044e2f6dfd5d8473b7e51b
patriciamesttre <patriciamesttre@hotmail.com>
2019-05-06 16:37:43 -0300

    Animação no Unity

---
+++ animacao/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    SpriteRenderer spritePlayer;
+    Animator animacao;
+
+    void Start()
+    {
+        spritePlayer = GetComponent<SpriteRenderer>();
+        animacao = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            animacao.SetBool("Andar", true);
+        }
+        else
+        {
+            animacao.SetBool("Andar", false);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 0.05f);
+            spritePlayer.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * 0.05f);
+            spritePlayer.flipX = true;
+        }
+    }
+}
